aa0ce2c8a6fa22a5c22bfddbd9f70de5ce57664d
imanwaheed317 <160356928+imanwaheed317@users.noreply.github.com>
2024-02-21 21:28:46 +0500

    Add files via upload

---
+++ Famous_quote_2.js
@@ -0,0 +1,4 @@
+"use strict";
+let Famous_person = 'Albert Einstine';
+let message = '"A person who never made a mistake never tried anything new"';
+console.log(`${Famous_person} once said,${message}`);
---
+++ Famous_quote_2.ts
@@ -0,0 +1,5 @@
+let Famous_person :string = 'Albert Einstine';
+
+let message :string = '"A person who never made a mistake never tried anything new"';
+
+console.log(`${Famous_person} once said,${message}`);
